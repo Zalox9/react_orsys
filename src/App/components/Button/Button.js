42d8677c4091a283e3ef84b62903e95b122eb0bc
Zalox9 <kv.paque@gmail.com>
2021-09-13 14:07:49 +0100

    add type Button

--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -1,11 +1,12 @@
 import React from 'react' ///imr <- raccourcie
+import { isPropertyAccessOrQualifiedName } from 'typescript';
 import './Button.css'
 
 
 function Button(props) { //const Button=()=>{}
 console.log(props);
     return (
-        <button className="Button">{props.children}</button>
+        <button className="Button" type={props.type}>{props.children}</button>
     );
 }
 
