cf3c442e41bb79f12a042124a6634f042a92f2bd
Zalox9 <kv.paque@gmail.com>
2021-09-13 15:56:42 +0100

    ajout fonction Button props

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -7,10 +7,16 @@ function App() {
     <div className="App">
       Hello world !
       <hr />
-      <Button bgColor="green">
+      <Button bgColor="green"
+      onClickEvent={(arg) =>{
+        console.trace(arg);
+      }}>
         <img src="/img/ok.png" alt="btn ok" />
       </Button>
-      <Button bgColor="tomato" style={{textDecoration:'underline', backgroundColor:'red'}}>
+      <Button bgColor="tomato" style={{textDecoration:'underline', backgroundColor:'red'}}
+      onClickEvent={(arg) =>{
+        console.error("il y'a une erreur ", arg)
+      }}>
         <img src="/img/cancel.png" alt="btn cancel" />
         Cancel
       </Button>
--- src/App/components/Button/Button.js
+++ src/App/components/Button/Button.js
@@ -7,22 +7,27 @@ import PropTypes from 'prop-types'
 function Button(props) { //const Button=()=>{}
 console.log(props);
     return (
-        <button className="Button"
+        <button className="Button "
         type={props.type}
         style={{backgroundColor:props.bgColor,...props.style}}
+        onClick={(evt)=>{
+            props.onClickEvent("blabla");
+        }} //arrow fonction perserve 
         >{props.children}</button>
     );
 }
 
-Button.propTypes={
+Button.propTypes={ // définit ce qu'est le props
     children:PropTypes.any.isRequired, //info console si aucun element dans children pt->PropTypes
     bgColor:PropTypes.string,
     style:PropTypes.object,
+    onClickEvent:PropTypes.func.isRequired,
 }
 
-Button.defaultProps={
-    children:"default button value", //valeur par défaut
+Button.defaultProps={ //valeur par défaut
+    children:"default button value", 
     bgColor:"DarkOrange",
+    onClickEvent:()=>{} //ne fais rien
 }
 
 
