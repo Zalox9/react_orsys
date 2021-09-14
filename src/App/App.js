0ed253bd02f5b8a09a2a1e3af78197a0ccdad1ff
Zalox9 <kv.paque@gmail.com>
2021-09-14 08:24:57 +0100

    add gestion etat

--- src/App/App.js
+++ src/App/App.js
@@ -3,20 +3,27 @@ import Button from './components/Button/Button' //imp <- raccourcie
 import './App.css';
 
 class App extends React.Component {
-    counter = 0;
+    // counter = 0;
+    constructor(props){
+        super(props);
+        this.state={counter:0};
+    }
+    
     render() {
         return (
             <div className="App">
-                Valeur du counter : {this.counter}
+                Valeur du counter : {this.state.counter}
                 <hr />
                 <Button onClickEvent={() => {
-                    this.counter--;
-                    console.log(this.counter)
+                    this.setState({counter:this.state.counter-1}) //edition etat async
+                    // this.state.counter--;
+                    console.log(this.state.counter)
                 }}
                 >soustraction</Button>
                 <Button onClickEvent={() => {
-                    this.counter++;
-                    console.log(this.counter)
+                    this.setState({counter:this.state.counter+1})
+                    // this.state.counter++;
+                    console.log(this.state.counter)
                 }}
                     bgColor="red">addition</Button>
             </div>
