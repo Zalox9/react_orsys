ba8a088c1ad6cab96b1f9f442f900bbe4931681c
Zalox9 <kv.paque@gmail.com>
2021-09-14 15:38:54 +0100

    correctif

--- src/App/components/FormMessage/FormMessage.js
+++ src/App/components/FormMessage/FormMessage.js
@@ -7,8 +7,8 @@ import MessageInput from '../MessageInput/MessageInput';
 import Button from '../Button/Button'
 
 export const FormMessageInitialState = {
-  text:'new text',
-  destId:0
+  text: 'new text',
+  destId: 0
 };
 
 const FormMessage = (props) => {
@@ -16,11 +16,11 @@ const FormMessage = (props) => {
   return (
     <div className={styles.FormMessage} data-testid="FormMessage">
       {JSON.stringify(FormMessageState)}
-      <form onSubmit={(evt)=>{
+      <form onSubmit={(evt) => {
         evt.preventDefault(); //empeche le rechargement de la page
       }}>
-        <MessageInput value={FormMessageState.text} onChange={(evt)=>{setFormMessageState({...FormMessageState, text:evt.target.value})}}></MessageInput>
-        <SelectUser value={FormMessageState.destId} users={userListInitialState} onChange={(evt)=>setFormMessageState({...FormMessageState, destId:Number(evt.target.value)})}></SelectUser>
+        <MessageInput value={FormMessageState.text} onChange={(evt) => { setFormMessageState({ ...FormMessageState, text: evt.target.value }) }}></MessageInput>
+        <SelectUser value={FormMessageState.destId} users={userListInitialState} onChange={(evt) => setFormMessageState({ ...FormMessageState, destId: Number(evt.target.value) })}></SelectUser>
         <Button type="submit">Envoyer</Button>
       </form>
     </div>
