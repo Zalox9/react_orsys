85d4834ab659a72c0ff02529e731896533f19ce4
Zalox9 <kv.paque@gmail.com>
2021-09-14 11:08:06 +0100

    config Tchat + FlexContainer

---
+++ src/App/components/Tchat/Tchat.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import FlexContainer from '../FlexContainer/FlexContainer';
+import FormMessage from '../FormMessage/FormMessage';
+import MessageList from '../MessageList/MessageList';
+import UserList from '../UserList/UserList';
+import styles from './Tchat.module.scss';
+
+const Tchat = () => (
+  <div className={styles.Tchat} data-testid="Tchat">
+    <FlexContainer type="horizontal" style={{height: "98vh"}}>
+      {/* partie supérieur */}
+      <FlexContainer type="vertical">
+        <MessageList />
+        <UserList />
+      </FlexContainer>
+      {/* partie supérieur */}
+      <FormMessage></FormMessage>
+    </FlexContainer>
+  </div>
+);
+
+Tchat.propTypes = {};
+
+Tchat.defaultProps = {};
+
+export default Tchat;
